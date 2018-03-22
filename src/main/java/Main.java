6d7a32cfad4dc858bed3307f7d4d84a2ff754155
SergeyPanov <panovsy@gmail.com>
2018-03-22 15:34:34 +0100

    Common-CLI parser used

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
-import com.beust.jcommander.JCommander;
 import instance.Instance;
-import params.ParamsParser;
+import params.Parameters;
 import solution.Environment;
 import solution.Solution;
 
@@ -8,33 +7,30 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws JAXBException {
 
+        Parameters params = new Parameters();
+        params.parse(args);
 
 
-        ParamsParser paramsParser = new ParamsParser();
-        JCommander.newBuilder().addObject(paramsParser).build().parse(args);
-
-        File file = new File(paramsParser.getPath());
+        File file = new File(params.getPath());
         JAXBContext jaxbContext = JAXBContext.newInstance(Instance.class);
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
         Instance instance = (Instance) jaxbUnmarshaller.unmarshal(file);
 
         Environment environment = new Environment(instance);
 
-        if (paramsParser.isCompare()){
+        if (params.isCompare()){
             Solution solution = new Solution(environment);
             solution.greedySearch();
             solution.printBestRoute();
         }
 
         Solution solution = new Solution(environment);
-        solution.tabuSearch(  paramsParser.getIterations());
+        solution.tabuSearch(  params.getIterations());
         solution.printBestRoute();
 
     }
---
+++ src/main/java/params/Parameters.java
@@ -0,0 +1,59 @@
+package params;
+
+import org.apache.commons.cli.*;
+
+
+public class Parameters {
+    private  Options options = new Options();
+
+
+    private String path;
+
+    private boolean isCompare;
+
+    private int iterations = 300;
+
+
+
+    public Parameters(){
+        Option optionPath = new Option("p", true, "Path to the file.");
+        Option optionCmp = new Option("c", false, "In case of set, the result of Greedy search will be printed.");
+        Option optionIters = new Option("i", true, "Set amount of iterations for Tabu Search");
+
+        options.addOption(optionPath);
+        options.addOption(optionCmp);
+        options.addOption(optionIters);
+    }
+
+    public void parse(String[] args){
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
+        try{
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        assert cmd != null;
+        path = cmd.getOptionValue("p");
+        isCompare = cmd.hasOption("c");
+        iterations = cmd.getOptionValue("i") == null ? 300 : Integer.parseInt(cmd.getOptionValue("i"));
+
+    }
+
+    public int getIterations() {
+        return iterations;
+    }
+
+
+    public boolean isCompare() {
+        return isCompare;
+    }
+
+
+    public String getPath() {
+        return path;
+    }
+}
